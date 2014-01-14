14d54bc54458321f449bb78fabf4413d214c99ca
zbiggy1982 <wmitchell1982@gmail.com>
2014-01-14 15:48:08 -0700

    commit

---
+++ PetName/nameEntryAndDisplay.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace PetName
+{
+    partial class nameEntryAndDisplay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnPetName = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.lblPetName = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.msitFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.msitExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.msitHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.msitAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.cbPetType = new System.Windows.Forms.ComboBox();
+            this.errInfo = new System.Windows.Forms.ErrorProvider(this.components);
+            this.ttInfo = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPetName
+            // 
+            this.btnPetName.Location = new System.Drawing.Point(12, 172);
+            this.btnPetName.Name = "btnPetName";
+            this.btnPetName.Size = new System.Drawing.Size(75, 23);
+            this.btnPetName.TabIndex = 0;
+            this.btnPetName.Text = "&Pet Name";
+            this.btnPetName.UseVisualStyleBackColor = true;
+            this.btnPetName.Click += new System.EventHandler(this.btnPetName_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(93, 172);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(174, 172);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(9, 58);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(233, 13);
+            this.lblInstructions.TabIndex = 3;
+            this.lblInstructions.Text = "Please Choose a Pet Type and Press Pet Name";
+            // 
+            // lblPetName
+            // 
+            this.lblPetName.AutoSize = true;
+            this.lblPetName.Location = new System.Drawing.Point(12, 135);
+            this.lblPetName.Name = "lblPetName";
+            this.lblPetName.Size = new System.Drawing.Size(35, 13);
+            this.lblPetName.TabIndex = 4;
+            this.lblPetName.Text = "label2";
+            this.lblPetName.Visible = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.msitFile,
+            this.msitHelp});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "msMain";
+            // 
+            // msitFile
+            // 
+            this.msitFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.msitExit});
+            this.msitFile.Name = "msitFile";
+            this.msitFile.Size = new System.Drawing.Size(37, 20);
+            this.msitFile.Text = "&File";
+            // 
+            // msitExit
+            // 
+            this.msitExit.Name = "msitExit";
+            this.msitExit.Size = new System.Drawing.Size(92, 22);
+            this.msitExit.Text = "E&xit";
+            this.msitExit.Click += new System.EventHandler(this.msitExit_Click);
+            // 
+            // msitHelp
+            // 
+            this.msitHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.msitAbout});
+            this.msitHelp.Name = "msitHelp";
+            this.msitHelp.Size = new System.Drawing.Size(44, 20);
+            this.msitHelp.Text = "&Help";
+            // 
+            // msitAbout
+            // 
+            this.msitAbout.Name = "msitAbout";
+            this.msitAbout.Size = new System.Drawing.Size(107, 22);
+            this.msitAbout.Text = "A&bout";
+            this.msitAbout.Click += new System.EventHandler(this.msitAbout_Click);
+            // 
+            // cbPetType
+            // 
+            this.cbPetType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPetType.FormattingEnabled = true;
+            this.cbPetType.Items.AddRange(new object[] {
+            "Dog",
+            "Cat",
+            "Fish"});
+            this.cbPetType.Location = new System.Drawing.Point(12, 91);
+            this.cbPetType.Name = "cbPetType";
+            this.cbPetType.Size = new System.Drawing.Size(237, 21);
+            this.cbPetType.TabIndex = 6;
+            // 
+            // errInfo
+            // 
+            this.errInfo.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errInfo.ContainerControl = this;
+            // 
+            // ttInfo
+            // 
+            this.ttInfo.IsBalloon = true;
+            // 
+            // nameEntryAndDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 218);
+            this.Controls.Add(this.cbPetType);
+            this.Controls.Add(this.lblPetName);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnPetName);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "nameEntryAndDisplay";
+            this.ShowIcon = false;
+            this.Text = "Pet Name Generator";
+            this.Load += new System.EventHandler(this.nameEntryAndDisplay_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPetName;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Label lblPetName;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem msitFile;
+        private System.Windows.Forms.ToolStripMenuItem msitExit;
+        private System.Windows.Forms.ToolStripMenuItem msitHelp;
+        private System.Windows.Forms.ToolStripMenuItem msitAbout;
+        private System.Windows.Forms.ComboBox cbPetType;
+        private System.Windows.Forms.ErrorProvider errInfo;
+        private System.Windows.Forms.ToolTip ttInfo;
+    }
+}
+
---
+++ PetName/nameEntryAndDisplay.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PetName
+{
+    
+    /// William Mitchell
+    /// POS/409
+    /// 12/16/2013
+    /// Dr Brad Purdy
+    /// Application: Pet Name Generator
+    /// This application will generate a random petname based on the type of pet the user choses
+            public partial class nameEntryAndDisplay : Form
+    {
+        
+        /// Initializes a new instance of the <see cref="nameEntryAndDisplay"/> class.
+        public nameEntryAndDisplay()
+        {
+            InitializeComponent();
+        }
+
+        /// Handles the Click event of the btnExit control.
+        /// calls the exit application method from the exit button
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            exitApplication();
+        }
+      
+        /// Exits the application.
+        private void exitApplication()
+        {
+            Application.Exit();
+        }
+
+        /// Handles the Click event of the msitExit control.
+        /// calls the exit application method from the exit menu item
+        private void msitExit_Click(object sender, EventArgs e)
+        {
+            exitApplication();
+        }
+
+        /// Handles the Click event of the btnReset control.
+        /// calls the reset form method from the reset button
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            resetForm();
+        }
+
+        /// Resets the form.
+        private void resetForm()
+        {
+            this.lblPetName.Visible = false;
+            this.lblPetName.Text = null;
+            this.errInfo.Clear();
+            this.cbPetType.SelectedIndex = -1;
+        }
+
+        /// Handles the Click event of the msitAbout control.
+        /// shows the about message box
+        private void msitAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Pet Name Generator By William Mitchell 2013", "About", MessageBoxButtons.OK, MessageBoxIcon.None);
+        }
+
+        /// Handles the Click event of the btnPetName control.
+        /// calls the generate name method
+        private void btnPetName_Click(object sender, EventArgs e)
+        {
+            generateName();
+        }
+
+        /// Generates the name.
+        private void generateName()
+        {
+            var petType = this.cbPetType.SelectedIndex;
+            
+            string[] catNames = { "Milo", "Tabby", "Snickers", "Kitty", "Bannana Bread", "Shasha", "Flower"}; //creates an array for possible cat names 
+            string[] fishNames = { "Blubber", "Bubbles", "Goldy", "Dot", "Nemo", "Dory", "Sharkbait" }; //creates an array for possible fish names 
+            string[] dogNames = { "Fido", "Rover", "Champ", "Rex", "Molly", "Max", "Ruby" }; //creates an array for possible dog names 
+            string petNameSelected; //creates string variable for the pet name selected from random picking selected
+            Random random = new Random(); //sets the random picker method to be used to generate a pet name
+
+            if (petType == -1)
+            {
+                this.errInfo.SetError(this.cbPetType, "Please Select a Pet Type"); //creates an error event if no pet is chosen
+            }
+            else if (petType == 0) //if a dog is chosen this will occer
+            {
+                string petTypeString = cbPetType.SelectedItem.ToString(); //assigns value to pet type string
+                petNameSelected = dogNames[random.Next(0,7)]; //randomly selects from the dog name array
+                this.lblPetName.Text = "Your " + petTypeString + "'s name is " + petNameSelected; //updates the label for the pet name selected
+                this.lblPetName.Visible = true; //shows the pet name label
+
+            }
+            else if (petType == 1) //if a cat is chosen this will occer
+            {
+                string petTypeString = cbPetType.SelectedItem.ToString(); //assigns value to pet type string
+                petNameSelected = catNames[random.Next(0, 7)]; //randomly selects from the cat name array
+                this.lblPetName.Text = "Your " + petTypeString + "'s name is " + petNameSelected; //updates the label for the pet name selected
+                this.lblPetName.Visible = true; //shows the pet name label
+            }
+            else if (petType == 2)
+            {
+                string petTypeString = cbPetType.SelectedItem.ToString(); //assigns value to pet type string
+                petNameSelected = fishNames[random.Next(0, 7)]; //randomly selects from the fish name array
+                this.lblPetName.Text = "Your " + petTypeString + "'s name is " + petNameSelected; //updates the label for the pet name selected
+                this.lblPetName.Visible = true; //shows the pet name label
+            }
+        }
+
+
+        /// Handles the Load event of the nameEntryAndDisplay control.
+        /// calls the tool tip creator method
+        private void nameEntryAndDisplay_Load(object sender, EventArgs e)
+        {
+            toolTipCreator();
+        }
+
+        /// Tool tip creator.
+        /// sets messages for the tool tips
+        private void toolTipCreator()
+        {
+            this.ttInfo.SetToolTip(this.cbPetType, "Choose a pet type");
+            this.ttInfo.SetToolTip(this.btnExit, "Exit Application");
+            this.ttInfo.SetToolTip(this.btnPetName, "Generate a pet's name");
+            this.ttInfo.SetToolTip(this.btnReset, "Reset the form");
+        }
+    
+    }
+
+
+}
